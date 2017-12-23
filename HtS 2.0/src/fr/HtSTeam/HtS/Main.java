250e76b2a9876ea89ad5d837eafcd0ce1a8634ba
theogiraudet <theo.giraudet@orange.fr>
2017-12-23 19:10:56 +0100

    s

--- HtS 2.0/src/fr/HtSTeam/HtS/Main.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Main.java
@@ -1,14 +1,20 @@
 package fr.HtSTeam.HtS;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.HtSTeam.HtS.Commands.CommandsManager;
 
 public class Main extends JavaPlugin {
+	
+	public HashMap<Player, UUID> uuidPlayer = new HashMap<>();
 
 	@Override
 	public void onEnable() {
