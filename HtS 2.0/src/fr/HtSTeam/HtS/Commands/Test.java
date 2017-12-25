945c36963ba03d1a610897e55441307598716dc6
user <user@computer.home>
2017-12-25 21:33:24 +0100

    update test

--- HtS 2.0/src/fr/HtSTeam/HtS/Commands/Test.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Commands/Test.java
@@ -6,7 +6,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import fr.HtSTeam.HtS.Main;
-import fr.HtSTeam.HtS.ScoreBoard;
 import fr.HtSTeam.HtS.Options.OptionsRegister;
 
 public class Test implements CommandExecutor {
@@ -24,7 +23,6 @@ public class Test implements CommandExecutor {
 			Player p = (Player) sender;
 			if (cmd.getName().equalsIgnoreCase("test") && sender.hasPermission("test.use")) {
 				OptionsRegister.main.open(p);
-				ScoreBoard.send(p);
 				return true;
 			}
 			
