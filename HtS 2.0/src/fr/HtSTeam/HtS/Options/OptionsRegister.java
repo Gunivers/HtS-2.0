a185d46d6c9e4573a2022ed3098d1fa5bed0a800
theogiraudet <theo.giraudet@orange.fr>
2017-12-25 21:38:16 +0100

    Update

--- HtS 2.0/src/fr/HtSTeam/HtS/Options/OptionsRegister.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Options/OptionsRegister.java
@@ -8,8 +8,7 @@ import fr.HtSTeam.HtS.Options.Structure.GUIManager;
 public class OptionsRegister {
 	
 	public static GUIManager main = new GUIManager("Options", 3, "Options", "Ouvre les options", Material.BARRIER, null);
-	public static GUIManager option1 = new GUIManager("Test", 1, "Test", "Ouvre les options 2", Material.SLIME_BALL, OptionsRegister.main);
-	public static GUIManager option2 = new GUIManager("Test 2", 1, "Test 2", "Ouvre les options 3", Material.DIAMOND, OptionsRegister.main);
+	public static GUIManager option1 = new GUIManager("UHC", 1, "UHC", "Régler les UHC", Material.GOLDEN_APPLE, OptionsRegister.main);
 
 	public static void register() {
 		new BorderOption();
