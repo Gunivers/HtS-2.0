5b5027ffd51d8c0838fdd1abb8780d282b55ddb6
theogiraudet <theo.giraudet@orange.fr>
2017-12-29 22:49:10 +0100

    Update

--- HtS 2.0/src/fr/HtSTeam/HtS/Commands/StartCommand.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Commands/StartCommand.java
@@ -4,7 +4,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import fr.HtSTeam.HtS.Main;
+
 public class StartCommand implements CommandExecutor {
+
+	public StartCommand(Main main) {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
