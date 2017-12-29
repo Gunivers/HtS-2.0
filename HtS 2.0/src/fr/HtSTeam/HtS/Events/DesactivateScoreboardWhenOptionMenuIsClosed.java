b308b759ff98c1416f947773e3dce0b670e1ac37
user <user@computer.home>
2017-12-29 23:44:58 +0100

    updt

--- HtS 2.0/src/fr/HtSTeam/HtS/Events/DesactivateScoreboardWhenOptionMenuIsClosed.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Events/DesactivateScoreboardWhenOptionMenuIsClosed.java
@@ -4,13 +4,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
+import fr.HtSTeam.HtS.EnumState;
 import fr.HtSTeam.HtS.Scoreboard.ScoreBoard;
 
 public class DesactivateScoreboardWhenOptionMenuIsClosed implements Listener {
 	
 	@EventHandler
 	public void onOptionsClose(InventoryCloseEvent e) {
-		if(e.getInventory().getTitle().equals("Options") || e.getInventory().getTitle().equals("Scoreboard")) {
+		if(EnumState.getState().equals(EnumState.WAIT) && (e.getInventory().getTitle().equals("Options") || e.getInventory().getTitle().equals("Scoreboard"))) {
 			if (ScoreBoard.scoreboards.containsKey(e.getPlayer())) {
 				ScoreBoard.scoreboards.get(e.getPlayer()).deactivate();
 				ScoreBoard.scoreboards.remove(e.getPlayer());
