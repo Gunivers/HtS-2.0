45dc66dbe50ac96dc96bbcc616b10b94db8f221f
user <user@computer.home>
2018-01-02 15:58:25 +0100

    cmd

--- HtS 2.0/src/fr/HtSTeam/HtS/Commands/TeamCommand.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Commands/TeamCommand.java
@@ -20,29 +20,36 @@ public class TeamCommand implements CommandExecutor {
 			Player p = (Player) sender;
 			if (cmd.getName().equalsIgnoreCase("team") && p.hasPermission("team.use") && args.length > 0) {
 				if (args[0].equalsIgnoreCase("add") && args.length > 2) {
-					TeamManager tm = new TeamManager(args[1], args[2]);
+					TeamManager tm = new TeamManager(args[1], args[2].toLowerCase());
 					if (args.length == 4)
 						tm.setFakeTeam(Boolean.valueOf(args[3]));
+					p.sendMessage("L'équipe " + ChatColor.valueOf(tm.getTeamColor().toUpperCase()) + tm.getTeamName() + " §ra été créee !");
 					return true;
 				} else if (args[0].equalsIgnoreCase("remove") && args.length == 2) {
 					if (TeamManager.nameTeam.containsKey(args[1])) {
 						TeamManager.nameTeam.get(args[1]).clearTeam();
+						p.sendMessage("L'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName() + " §ra été supprimée !");
 						return true;
 					}
 					return false;
 				} else if (args[0].equalsIgnoreCase("join") && args.length == 3) {
 					if (TeamManager.nameTeam.containsKey(args[1])) {
 						TeamManager.nameTeam.get(args[1]).addPlayer(Bukkit.getPlayer(args[2]));
+						p.sendMessage(args[2] + " a rejoint l'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName());
 						return true;
 					}
 					return false;
 				} else if (args[0].equalsIgnoreCase("leave") && args.length == 2) {
-					TeamManager.playerTeam.get(Bukkit.getPlayer(args[1])).removePlayer(Bukkit.getPlayer(args[1]));
-					return true;
+					if (TeamManager.nameTeam.containsKey(args[1])) {
+						TeamManager.playerTeam.get(Bukkit.getPlayer(args[1])).removePlayer(Bukkit.getPlayer(args[1]));
+						p.sendMessage(args[2] + " a quitté l'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName());
+						return true;
+					}
+					return false;
 				} else if (args[0].equalsIgnoreCase("list") && args.length == 1) {
-					p.sendMessage("§6List des Teams:");
+					p.sendMessage("§6Liste des Teams:");
 					for(TeamManager t :TeamManager.teamList)
-						p.sendMessage("- " + ChatColor.valueOf(t.getTeamColor()) + t.getTeamName());
+						p.sendMessage("- " + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName());
 					return true;
 				} else if (args[0].equalsIgnoreCase("random")) {
 					if(TeamManager.teamList.size() != 0) {
