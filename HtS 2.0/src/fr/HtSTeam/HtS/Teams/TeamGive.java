82322675e521b272801218e55ff0cf0d20fc71e2
user <user@computer.home>
2018-01-02 20:39:26 +0100

    team

--- HtS 2.0/src/fr/HtSTeam/HtS/Teams/TeamGive.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Teams/TeamGive.java
@@ -11,19 +11,21 @@ public class TeamGive implements Listener {
 	
 	@EventHandler
 	public void onWoolClicked(PlayerInteractEvent e) {
-		if (e.getItem().getType().equals(Material.WOOL) && e.getItem().getItemMeta().hasEnchants() && (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
-			e.setCancelled(true);
-			if (TeamManager.playerTeam.containsKey(e.getPlayer().getUniqueId())) {
-				if (TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamSize() == 1) {
-					TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).removePlayer(e.getPlayer());
-				} else {
-					e.getPlayer().sendMessage("Vous ne pouvez pas quitter l'équipe " + ChatColor.valueOf(TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamColor().toUpperCase()) + TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamName());
-					return;
+		if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			if (e.getItem().getType().equals(Material.WOOL) && e.getItem().getItemMeta().hasEnchants()) {
+				e.setCancelled(true);
+				if (TeamManager.playerTeam.containsKey(e.getPlayer().getUniqueId())) {
+					if (TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamSize() > 1) {
+						TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).removePlayer(e.getPlayer());
+					} else {
+						e.getPlayer().sendMessage("Vous ne pouvez pas quitter l'équipe "+ ChatColor.valueOf(TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamColor().toUpperCase())+ TeamManager.playerTeam.get(e.getPlayer().getUniqueId()).getTeamName());
+						return;
+					}
 				}
+
+				if (TeamManager.nameTeam.containsKey(e.getItem().getItemMeta().getDisplayName().substring(2)))
+					TeamManager.nameTeam.get(e.getItem().getItemMeta().getDisplayName().substring(2)).addPlayer(e.getPlayer());
 			}
-			
-			if (TeamManager.nameTeam.containsKey(e.getItem().getItemMeta().getDisplayName().substring(2)))
-				TeamManager.nameTeam.get(e.getItem().getItemMeta().getDisplayName().substring(2)).addPlayer(e.getPlayer());
 		}
 	}
 }
