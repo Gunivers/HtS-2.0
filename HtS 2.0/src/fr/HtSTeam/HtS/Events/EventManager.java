b28104eb46231ff4eb76d5a2b8109d6f1911b5e1
theogiraudet <theo.giraudet@orange.fr>
2018-01-02 23:45:37 +0100

    Fixe

--- HtS 2.0/src/fr/HtSTeam/HtS/Events/EventManager.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Events/EventManager.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.PluginManager;
 import fr.HtSTeam.HtS.Main;
 import fr.HtSTeam.HtS.Players.PlayerInGame;
 import fr.HtSTeam.HtS.Players.Spectator.CustomChat;
+import fr.HtSTeam.HtS.Players.Spectator.SeeInventory;
 import fr.HtSTeam.HtS.Teams.TeamGive;
 
 public class EventManager {
@@ -21,6 +22,7 @@ public class EventManager {
 		pm.registerEvents(new WaitEvent(), main);
 		pm.registerEvents(new CustomChat(), main);
 		pm.registerEvents(new ReloadServerEvent(), main);
+		pm.registerEvents(new SeeInventory(), main);
 	}
 
 }
