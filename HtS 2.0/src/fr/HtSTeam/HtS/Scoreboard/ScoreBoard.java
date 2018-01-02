5c6eddab0ddf23ef8c000195e5afd063383e992c
user <user@computer.home>
2018-01-02 21:50:48 +0100

    sb

--- HtS 2.0/src/fr/HtSTeam/HtS/Scoreboard/ScoreBoard.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Scoreboard/ScoreBoard.java
@@ -7,11 +7,13 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Statistic;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 import fr.HtSTeam.HtS.Main;
 import fr.HtSTeam.HtS.Commands.PlayStopCommands;
 import fr.HtSTeam.HtS.Options.OptionsRegister;
+import fr.HtSTeam.HtS.Options.Options.GameMode.GameModeState;
 import fr.HtSTeam.HtS.Players.PlayerInGame;
 import fr.HtSTeam.HtS.Scoreboard.Scoreboard.Entry;
 import fr.HtSTeam.HtS.Scoreboard.Scoreboard.EntryBuilder;
@@ -26,6 +28,7 @@ public class ScoreBoard {
 	
 	public static ArrayList<String> display = new ArrayList<String>();
 	public static Map<UUID, Scoreboard> scoreboards = new HashMap<UUID, Scoreboard>();
+	private final static ScrollableString scroll = new ScrollableString(Strings.format("§4JEU EN PAUSE"), 40, 0);
 		
 	public static void send(Player player) {
 
@@ -63,8 +66,7 @@ public class ScoreBoard {
 					break;
 				case "TimerScoreboardOption":
 					builder.next("§6Timer:").next(Main.timer.getTimeFormat());
-					System.out.println(Main.gamemode.toString());
-					if (Main.gamemode.toString().equals("Fallen Kingdom"))
+					if (((GameModeState) Main.gamemode).getGameMode().equals("Fallen Kingdom"))
 						builder.next("§6Days:").next(Integer.toString((int) Main.timer.getTimerInMinute() / 20));
 					break;
 				case "BorderScoreboardOption":
@@ -77,7 +79,7 @@ public class ScoreBoard {
 		}
 		
 		if (PlayStopCommands.pause)
-			builder.blank().blank().next("	" + new ScrollableString(Strings.format("§4JEU EN PAUSE"), 40, 0).next());
+			builder.blank().blank().next("		" + scroll.next());
 		
 		return builder.build();
 	}
