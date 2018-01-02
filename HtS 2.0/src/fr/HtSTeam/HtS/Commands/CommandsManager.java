b29e41eef988b66c985f4440454b0a34c5c19289
user <user@computer.home>
2018-01-02 15:31:43 +0100

    teamcommand

--- HtS 2.0/src/fr/HtSTeam/HtS/Commands/CommandsManager.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Commands/CommandsManager.java
@@ -8,7 +8,7 @@ public class CommandsManager {
 		main.getCommand("option").setExecutor(new OptionCommand(main));
 		main.getCommand("start").setExecutor(new StartCommand(main));
 		main.getCommand("run").setExecutor(new StartCommand(main));
-		
+		main.getCommand("team").setExecutor(new TeamCommand());
 	}
 
 }
