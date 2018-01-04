1a4d048a3822599505f681dd7c9f55c682cdc16f
user <user@computer.home>
2018-01-04 21:57:11 +0100

    base

--- HtS 2.0/src/fr/HtSTeam/HtS/GameModes/FallenKingdom/PlayerBase.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/GameModes/FallenKingdom/PlayerBase.java
@@ -20,7 +20,7 @@ public enum PlayerBase {
 	public static PlayerBase isInBase(Player p, Location l) {
 		for (BaseManager b : BaseManager.baseList) {
 			int pos[][] = b.getOrderPos();
-			if (pos[0][0] < l.getBlockX() && l.getBlockX() < pos[1][0] && pos[0][1] < l.getBlockZ() && l.getBlockZ() < pos[1][1]) {
+			if (pos[0][0] < l.getBlockX() && l.getBlockX() < pos[0][1] && pos[1][0] < l.getBlockZ() && l.getBlockZ() < pos[1][1]) {
 				if(BaseManager.playerBase.get(p.getUniqueId()) == b)
 					return PlayerBase.OWN;
 				else if (b.isNeutral())
