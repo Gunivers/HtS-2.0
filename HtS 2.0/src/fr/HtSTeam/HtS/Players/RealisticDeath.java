5725f784e344b2599168f9443e211ec7e2d0f019
user <user@computer.home>
2018-01-04 15:37:38 +0100

    realisticdeath

--- HtS 2.0/src/fr/HtSTeam/HtS/Players/RealisticDeath.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Players/RealisticDeath.java
@@ -1,7 +1,6 @@
 package fr.HtSTeam.HtS.Players;
 
 import org.bukkit.Location;
-import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,16 +14,24 @@ public class RealisticDeath implements Listener {
 	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onDeath(PlayerDeathEvent e) {
+		e.getDrops().clear();
 		realisticDeath(e.getEntity(), e.getEntity().getLocation());
 	}
 
 	private void realisticDeath(Player p, Location l) {
-		 ArmorStand am = (ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
-		 am.setArms(true);
-		 am.setBasePlate(false);
-		 am.setGravity(true);
-		 am.setInvulnerable(true);
-		 am.setItemInHand(p.getEquipment().getItemInMainHand());
-		 am.setVisible(true);
+		Player e = (Player) l.getWorld().spawnEntity(l, EntityType.PLAYER);
+		e.setAI(false);
+		e.setCanPickupItems(false);
+		e.setCollidable(true);
+		e.setCustomNameVisible(false);
+		e.setGravity(true);
+		e.setSilent(true);
+		e.setInvulnerable(true);
+		e.getEquipment().setItemInMainHand(p.getEquipment().getItemInMainHand());
+		e.getEquipment().setItemInOffHand(p.getEquipment().getItemInOffHand());
+		e.getEquipment().setHelmet(p.getEquipment().getHelmet());
+		e.getEquipment().setChestplate(p.getEquipment().getChestplate());
+		e.getEquipment().setLeggings(p.getEquipment().getLeggings());
+		e.getEquipment().setBoots(p.getEquipment().getBoots());
 	}
 }
