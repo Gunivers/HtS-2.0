8d15010733c87d724260b538d3a5b9f240541c1d
theogiraudet <theo.giraudet@orange.fr>
2018-01-04 22:07:19 +0100

    Fixe

--- HtS 2.0/src/fr/HtSTeam/HtS/GameModes/FallenKingdom/CakeFirstGame.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/GameModes/FallenKingdom/CakeFirstGame.java
@@ -12,8 +12,6 @@ public class CakeFirstGame implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onConcretePlace(BlockPlaceEvent e) {
-		System.out.println(e.getBlock().getType().equals(Material.CAKE_BLOCK) && e.getBlockAgainst().getType().equals(Material.CONCRETE));
-		System.out.println(e.getBlockAgainst().getType());
 		if(e.getBlock().getType().equals(Material.CAKE_BLOCK) && e.getBlockAgainst().getType().equals(Material.CONCRETE))
 			if(e.getBlockAgainst().getData() == 4 || e.getBlockAgainst().getData() == 5 || e.getBlockAgainst().getData() == 14) {
 				e.setCancelled(false);
