f605ea1559d55fb37e1c044281dbc76a4534df5a
user <user@computer.home>
2018-01-07 16:29:28 +0100

    fix

--- HtS 2.0/src/fr/HtSTeam/HtS/Events/JoinLeaveEvent.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Events/JoinLeaveEvent.java
@@ -1,12 +1,16 @@
 package fr.HtSTeam.HtS.Events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.HtSTeam.HtS.EnumState;
 import fr.HtSTeam.HtS.Scoreboard.ScoreBoard;
+import fr.HtSTeam.HtS.Teams.TeamManager;
 
 public class JoinLeaveEvent implements Listener {
 	
@@ -15,8 +19,15 @@ public class JoinLeaveEvent implements Listener {
 		Player p = e.getPlayer();
 		
 		if (EnumState.getState() == EnumState.RUNNING) {
-			ScoreBoard.scoreboards.get(p.getUniqueId()).activate();
-		}
+			ScoreBoard.send(p);
+			if (TeamManager.teamList.size() != 0 && p.getGameMode() != GameMode.SPECTATOR)
+				p.setDisplayName(ChatColor.valueOf(TeamManager.playerTeam.get(p.getUniqueId()).getTeamColor().toUpperCase()) + p.getName());
+		}	
 	}
 	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent e) {
+		if(EnumState.getState() == EnumState.RUNNING)
+			ScoreBoard.scoreboards.remove(e.getPlayer().getUniqueId());
+	}
 }
