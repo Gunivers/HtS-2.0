d0a82f1c72fe6992e09cd9b01c549ebf94403d98
user <user@computer.home>
2018-01-07 13:47:00 +0100

    teamcmd

--- HtS 2.0/src/fr/HtSTeam/HtS/Teams/TeamCommand.java
+++ HtS 2.0/src/fr/HtSTeam/HtS/Teams/TeamCommand.java
@@ -1,10 +1,10 @@
 package fr.HtSTeam.HtS.Teams;
 
 import java.util.ArrayList;
-import java.util.Map.Entry;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -23,58 +23,77 @@ public class TeamCommand implements CommandExecutor {
 			Player p = (Player) sender;
 			if (cmd.getName().equalsIgnoreCase("team") && p.hasPermission("team.use") && args.length > 0) {
 				if (args[0].equalsIgnoreCase("add") && args.length > 2) {
-					TeamManager tm = new TeamManager(args[1], args[2].toLowerCase());
-					if (args.length == 4)
-						tm.setFakeTeam(Boolean.valueOf(args[3]));
-					p.sendMessage("L'équipe " + ChatColor.valueOf(tm.getTeamColor().toUpperCase()) + tm.getTeamName() + " §ra été créée !");
-					return true;
+					try {
+						TeamManager tm = new TeamManager(args[1], args[2].toLowerCase());
+						if (args.length == 4)
+							tm.setFakeTeam(Boolean.valueOf(args[3]));
+						p.sendMessage("L'équipe " + ChatColor.valueOf(tm.getTeamColor().toUpperCase()) + tm.getTeamName() + " §ra été créée !");
+						return true;
+					} catch (Exception e){
+						p.sendMessage("§4Erreur !");
+						return false;
+					}
 				} else if (args[0].equalsIgnoreCase("remove") && args.length == 2) {
-					if (TeamManager.nameTeam.containsKey(args[1])) {
+					try {
 						p.sendMessage("L'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName() + " §ra été supprimée !");
 						TeamManager.nameTeam.get(args[1]).clearTeam();
 						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Equipe non valide !");
+						return false;
 					}
-					return false;
 				} else if (args[0].equalsIgnoreCase("join") && args.length == 3) {
-					if (TeamManager.nameTeam.containsKey(args[1])) {
+					try {
 						TeamManager.nameTeam.get(args[1]).addPlayer(Bukkit.getPlayer(args[2]));
 						p.sendMessage(args[2] + " a rejoint l'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName());
 						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Equipe ou joueur non valide !");
+						return false;
 					}
-					return false;
 				} else if (args[0].equalsIgnoreCase("leave") && args.length == 3) {
-					if (TeamManager.nameTeam.containsKey(args[1])) {
+					try {
 						TeamManager.nameTeam.get(args[1]).removePlayer(Bukkit.getPlayer(args[2]));
 						p.sendMessage(args[2] + " a quitté l'équipe " + ChatColor.valueOf(TeamManager.nameTeam.get(args[1]).getTeamColor().toUpperCase()) + TeamManager.nameTeam.get(args[1]).getTeamName());
 						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Equipe ou joueur non valide !");
+						return false;
 					}
-					return false;
 				} else if (args[0].equalsIgnoreCase("list") && args.length == 1) {
-					p.sendMessage("Liste des Teams:");
-					for(TeamManager t :TeamManager.teamList)
-						p.sendMessage("- " + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName());
-					return true;
-				} else if (args[0].equalsIgnoreCase("list") && args.length == 2) {
-					for(TeamManager tm : TeamManager.teamList) {
-						if(tm.getTeamName().equalsIgnoreCase(args[1])) {
-							p.sendMessage("Joueurs de la team" + ChatColor.valueOf(tm.getTeamColor().toUpperCase()) + tm.getTeamName() + " : " );
-							for(Entry<UUID, TeamManager> entry : TeamManager.playerTeam.entrySet())
-								if(entry.getValue().equals(tm))
-									p.sendMessage("- " + ChatColor.valueOf(tm.getTeamColor().toUpperCase()) + Bukkit.getPlayer(entry.getKey()).getName());
-							return true;
-						}
-						p.sendMessage("§4Team innexistante !");
+					try {
+						p.sendMessage("Liste des Teams:");
+						for(TeamManager t :TeamManager.teamList)
+							p.sendMessage("- " + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName());
 						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Aucunes équipes existantes !");
+						return false;
 					}
-					
-					return true;
+				} else if (args[0].equalsIgnoreCase("list") && args.length == 2) {
+					try {
+						TeamManager t = TeamManager.nameTeam.get(args[1]);
+						p.sendMessage("Joueurs de la team" + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName() + " : " );
+						for(UUID uuid : t.getTeamPlayers())
+							p.sendMessage("- " + Bukkit.getPlayer(uuid).getName());
+						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Equipe inexistante !");
+						return false;
+					}
 				} else if (args[0].equalsIgnoreCase("give") && args.length == 1) {
-					for (Player player : Bukkit.getOnlinePlayers())
-						player.getInventory().clear();
-					for (TeamManager t : TeamManager.teamList)
+					try {
 						for (Player player : Bukkit.getOnlinePlayers())
-							player.getInventory().addItem(new ItemStackManager(Material.WOOL, t.getTeamByte(), 1, ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName(), "§fClique pour rejoindre l'équipe " + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName(), true).getItemStack());
-					return true;
+							player.getInventory().clear();
+						for (TeamManager t : TeamManager.teamList)
+							for (Player player : Bukkit.getOnlinePlayers())
+								if (player.getGameMode().equals(GameMode.SPECTATOR))	
+									player.getInventory().addItem(new ItemStackManager(Material.WOOL, t.getTeamByte(), 1, ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName(), "§fClique pour rejoindre l'équipe " + ChatColor.valueOf(t.getTeamColor().toUpperCase()) + t.getTeamName(), true).getItemStack());
+						return true;
+					} catch (NullPointerException e) {
+						p.sendMessage("§4Aucunes équipes existantes !");
+						return false;
+					}
 				} else if (args[0].equalsIgnoreCase("random")) {
 					if(TeamManager.teamList.size() != 0) {
 						ArrayList<TeamManager> teamList = new ArrayList<TeamManager>();
@@ -92,6 +111,7 @@ public class TeamCommand implements CommandExecutor {
 						}
 						return true;
 					}
+					p.sendMessage("§4Aucunes équipes existantes !");
 					return false;
 				} else {
 					return false;
