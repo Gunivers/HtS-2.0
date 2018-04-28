ab767cf6067069f5cd2beb53e0a6bebc310fd1bd
ikbrunel <ikbrunel.coding@gmail.com>
2018-04-28 15:24:27 +0200

    V fix message

--- src/main/java/fr/HtSTeam/HtS/GameModes/UHC/Common/VictoryDetectionEvent.java
+++ src/main/java/fr/HtSTeam/HtS/GameModes/UHC/Common/VictoryDetectionEvent.java
@@ -1,6 +1,7 @@
 package fr.HtSTeam.HtS.GameModes.UHC.Common;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -23,7 +24,7 @@ public class VictoryDetectionEvent implements Listener {
 	public void onPlayerDeath(PlayerDeathEvent e) {
 		if(teamVictoryDetection && TeamBuilder.teamList.size() == 1) {
 			EnumState.setState(EnumState.FINISHING);
-			JSON.sendAll(TeamBuilder.teamList.get(0).getTeamColor() + "La team" + TeamBuilder.teamList.get(0).getTeamName() + " a gagné !", null, 5);
+			JSON.sendAll(ChatColor.valueOf(TeamBuilder.teamList.get(0).getTeamColor().toUpperCase()) + "La team " + TeamBuilder.teamList.get(0).getTeamName() + " a gagné !", null, 5);
 		} else if(PlayerInGame.playerInGame.size() == 1) {
 			EnumState.setState(EnumState.FINISHING);
 			JSON.sendAll(Bukkit.getPlayer(PlayerInGame.playerInGame.get(0)).getName() + " §2 a gagné !", null, 5);
