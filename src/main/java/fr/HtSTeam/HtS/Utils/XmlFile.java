4bc37c727eff1004ad9db0ed676151298175057c
user <user@computer.home>
2018-06-13 11:05:22 +0200

    XmlFile

--- src/main/java/fr/HtSTeam/HtS/Utils/XmlFile.java
+++ src/main/java/fr/HtSTeam/HtS/Utils/XmlFile.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -13,7 +14,6 @@ import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -26,8 +26,8 @@ public class XmlFile {
 	private File file;
 	private Document doc;
 
-	public XmlFile(JavaPlugin plugin, String fileName) {
-		file = new File(plugin.getDataFolder(), fileName + ".xml");
+	public XmlFile(String fileName) {
+		file = new File("C:\\Users\\user\\Downloads", fileName + ".xml");
 		if (file.exists()) {
 			try {
 				doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
@@ -47,10 +47,15 @@ public class XmlFile {
 		if (!file.exists())
 			file.getParentFile().mkdirs();
 		try {
-			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			Transformer tf = TransformerFactory.newInstance().newTransformer();
+			tf.setOutputProperty(OutputKeys.INDENT, "yes");
+			tf.setOutputProperty(OutputKeys.METHOD, "xml");
+			tf.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(file);
-			transformer.transform(source, result);
+			
+			tf.transform(source, result);
 		} catch (TransformerFactoryConfigurationError | TransformerException e) {
 			e.printStackTrace();
 		}
@@ -65,17 +70,20 @@ public class XmlFile {
 	}
 	
 	public String getValue(final Node node) {
-		return ((Element) node).getNodeValue();
+		return ((Element) node).getTextContent();
 	}
 	
 	public void set(final String node_name, final Map<String, String> attributes, final String node_value) {
 		Element root = doc.createElement(node_name);
 		doc.appendChild(root);
-		for (String atrr_name : attributes.keySet()) {
-			Attr attr = doc.createAttribute(atrr_name);
-			attr.setValue(attributes.get(atrr_name));
-			root.setAttributeNode(attr);
-		}
+		if (node_value != null)
+			root.appendChild(doc.createTextNode(node_value));
+		if (attributes != null)	
+			for (String atrr_name : attributes.keySet()) {
+				Attr attr = doc.createAttribute(atrr_name);
+				attr.setValue(attributes.get(atrr_name));
+				root.setAttributeNode(attr);
+			}
 	}
 
 	public void set(final String node_name, final Map<String, String> attributes, final String node_value,
@@ -84,16 +92,18 @@ public class XmlFile {
 			return;
 
 		Element node = doc.createElement(node_name);
-		node.appendChild(doc.createTextNode(node_value));
-		for (String atrr_name : attributes.keySet()) {
-			Attr attr = doc.createAttribute(atrr_name);
-			attr.setValue(attributes.get(atrr_name));
-			node.setAttributeNode(attr);
-		}
+		if (node_value != null)
+			node.appendChild(doc.createTextNode(node_value));
+		if(attributes != null)
+			for (String atrr_name : attributes.keySet()) {
+				Attr attr = doc.createAttribute(atrr_name);
+				attr.setValue(attributes.get(atrr_name));
+				node.setAttributeNode(attr);
+			}
 		NodeList parents = doc.getElementsByTagName(parent_name);
 		for (int i = 0; i < parents.getLength(); i++)
 			if (parents.item(i).getNodeType() == Node.ELEMENT_NODE)
-				if (((Element) parents.item(i)).getAttribute(parent_attr) == parent_attrvalue)
+				if (((Element) parents.item(i)).getAttribute(parent_attr) == parent_attrvalue || (parent_attr == null && parent_attrvalue == null))
 					((Element) parents.item(i)).appendChild(node);
 	}
 }
