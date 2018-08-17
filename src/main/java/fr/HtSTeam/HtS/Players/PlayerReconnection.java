4c3b6ef5a9316d87e2396631c69955a84d9737a9
ikbruneldev <ikbrunel.coding@gmail.com>
2018-08-17 20:04:56 +0200

    PlayerReconnect

--- src/main/java/fr/HtSTeam/HtS/Players/PlayerReconnection.java
+++ src/main/java/fr/HtSTeam/HtS/Players/PlayerReconnection.java
@@ -1,22 +1,31 @@
 package fr.HtSTeam.HtS.Players;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.UUID;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import fr.HtSTeam.HtS.Options.Structure.Annotation.AwaitingPlayer;
 
 public class PlayerReconnection implements Listener {
 
 	private HashMap<UUID, ArrayList<Object>> waitingList = new HashMap<UUID, ArrayList<Object>>();
 	
+	@EventHandler
 	public void onPlayerReconnect(PlayerJoinEvent e) {
-		for(Entry<UUID, ArrayList<Object>> entry : waitingList.entrySet()) {
-			for(Object o : entry.getValue()) {
-				
-			}
+		for (Object o : waitingList.get(e.getPlayer().getUniqueId())) {
+			if (!(o instanceof Class))
+				continue;
+			for (Method m : o.getClass().getMethods())
+				if (m.isAnnotationPresent(AwaitingPlayer.class))
+					try {
+						m.invoke(o, e.getPlayer().getUniqueId());
+					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) { ex.printStackTrace(); }
 		}
 	}
 }
