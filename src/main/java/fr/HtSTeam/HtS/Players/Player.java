11ead262059be634269734f8280839b6bad046b0
ikbruneldev <ikbrunel.coding@gmail.com>
2018-09-03 11:20:43 +0200

    security fix + example (sendMessage)

--- src/main/java/fr/HtSTeam/HtS/Players/Player.java
+++ src/main/java/fr/HtSTeam/HtS/Players/Player.java
@@ -16,14 +16,12 @@ public class Player {
 	
 	private static ArrayList<Player> players = new ArrayList<Player>();
 	private static HashMap<UUID, Player> uuids = new HashMap<UUID, Player>();
-	
 	/**
 	 * Returns the list of all Player (not neceseraly online).
 	 * 
 	 * @return List of Player
 	 */
 	public static ArrayList<Player> getPlayers() { return players; }
-	
 	/**
 	 * Returns the instance of Player from a UniqueId.
 	 * Should not return null.
@@ -34,14 +32,49 @@ public class Player {
 	 */
 	public static Player getPlayerFromUUID(UUID uuid) { if (uuids.containsKey(uuid)) return uuids.get(uuid); return null; }
 	
+	
 	private ArrayList<Runnable> asynctasks = new ArrayList<Runnable>();
+	
 	
 	private UUID uuid;
 	private String name;
 	private String display_name;
-
+	/**
+	 * Returns the UUID of this Player.
+	 * 
+	 * @return UUID
+	 */
+	public UUID getUUID() { return uuid; }
+	/**
+	 * Returns the UUID of this Player.
+	 * 
+	 * @return String
+	 */
+	public String getName() { return name; }
+	/**
+	 * Returns the displayed name of this Player.
+	 * 
+	 * @return String
+	 */
+	public String getDisplay() { return display_name; }
+	
+	
 	private TeamBuilder team;
 	private TeamBuilder fake_team;
+	/**
+	 * Returns the team of this Player.
+	 * 
+	 * @return TeamBuilder
+	 */
+	public TeamBuilder getTeam() { return team; }
+	
+	/**
+	 * Returns the fake team of this Player.
+	 * 
+	 * @return TeamBuilder
+	 */
+	public TeamBuilder getFakeTeam() { return fake_team; }
+	
 	
 	/**
 	 * Creates an independent player, highly modular and safe.
@@ -58,7 +91,6 @@ public class Player {
 		name = player.getName();
 		display_name = player.getDisplayName();
 	}
-	
 	/**
 	 * Creates an independent player, highly modular and safe. Cannot instantiate twice. 
 	 * 
@@ -72,46 +104,12 @@ public class Player {
 		return new Player(player);	
 	}
 	
-	/**
-	 * Returns the UUID of this Player.
-	 * 
-	 * @return UUID
-	 */
-	public UUID getUUID() { return uuid; }
 	
 	/**
-	 * Returns the UUID of this Player.
-	 * 
-	 * @return String
-	 */
-	public String getName() { return name; }
-	
-	/**
-	 * Returns the displayed name of this Player.
-	 * 
-	 * @return String
-	 */
-	public String getDisplay() { return display_name; }
-	
-	/**
-	 * Returns the team of this Player.
-	 * 
-	 * @return TeamBuilder
-	 */
-	public TeamBuilder getTeam() { return team; }
-	
-	/**
-	 * Returns the fake team of this Player.
-	 * 
-	 * @return TeamBuilder
-	 */
-	public TeamBuilder getFakeTeam() { return fake_team; }
-	
-	/**
-	 * Use this method to whether you should run your method in case the player is offline. You can add your method to beexecuted when the player reconnects
+	 * Use this method to whether you should run your method in case the player is offline. You can add your method to be executed when the player reconnects
 	 * 	
 	 * @param addasync should it add this method to run at the player reconnection
-	 * @param args parameters' values of method
+	 * @param args parameters' values of method (do not put addasync in args)
 	 * @return
 	 * 		true when the player is online <p></p>
 	 * 		false when the player is offline
@@ -125,17 +123,16 @@ public class Player {
 			return false;
 		}
 	}
-	
 	/**
 	 * Add a method to be run when the player reconnects  
 	 * 
 	 * @param args
-	 * 		parameters' values of method to run
+	 * 		parameters' values of method to run (do not put addasync in args)
 	 */
 	private void addAsyncTask(final Object... args) {
 		Method m = null;
 		for (Method o : getClass().getMethods())
-			if (o.getName().equals(Thread.currentThread().getStackTrace()[3].getMethodName()))
+			if (o.getName().equals(Thread.currentThread().getStackTrace()[3].getMethodName()) && o.getParameterCount() == args.length)
 				m = o;
 		
 		if (m == null)
@@ -150,8 +147,7 @@ public class Player {
 			}
 		};
 		asynctasks.add(runnable);
-	}
-	
+	}	
 	/**
 	 * Runs every method added via {@link #addAsyncTask(Object...) addAsyncTask}
 	 */
@@ -161,7 +157,6 @@ public class Player {
 		asynctasks.forEach(task -> task.run());
 		asynctasks.clear();
 	}
-	
 	/**
 	 * Event triggered when a player joined, executes the {@link #runAsyncTask() runAsyncTask} method
 	 * 
@@ -170,5 +165,25 @@ public class Player {
 	@EventHandler
 	public static void onPlayerjoin(PlayerJoinEvent e) {
 		getPlayerFromUUID(e.getPlayer().getUniqueId()).runAsyncTask();
+	}
+	
+	
+	
+	/**
+	 * Sends a message to the player if he is connected, else it will send it when he reconnects
+	 * 
+	 * @param message the message to send to the player
+	 */
+	public void sendMessage(String message) { sendMessage(message, true); }
+	/**
+	 * Sends a message to the player
+	 * 
+	 * @param message the message to send to the player
+	 * @param addasync whether it should send the message when the player reconnects in case he was offline
+	 */
+	public void sendMessage(String message, boolean addasync) {
+		if (!canExecute(addasync, message))
+			return;
+		Bukkit.getPlayer(uuid).sendMessage(message);
 	}
 }
