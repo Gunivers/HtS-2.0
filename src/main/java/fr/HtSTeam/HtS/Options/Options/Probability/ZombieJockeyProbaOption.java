1ba740f1ab3f9e9a112b2f73575ad00ed102da2e
A:Z <justin.carel@free.fr>
2018-09-22 16:28:16 +0200

    Add ZombieJockeyProbaOption

---
+++ src/main/java/fr/HtSTeam/HtS/Options/Options/Probability/ZombieJockeyProbaOption.java
@@ -0,0 +1,31 @@
+package fr.HtSTeam.HtS.Options.Options.Probability;
+
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+import fr.HtSTeam.HtS.Options.Options.Probability.Structure.Probability;
+
+/**
+ * 
+ * @author A~Z
+ *
+ */
+public class ZombieJockeyProbaOption extends Probability
+{
+	public ZombieJockeyProbaOption()
+	{
+		super(Material.BONE, "Spider Jockey", "§61.0§2%", 1.d);
+	}
+	
+	@EventHandler
+	public void onMobSpawn(CreatureSpawnEvent event)
+	{
+		if (event.getEntityType() == EntityType.ZOMBIE && random.nextDouble() < this.getValue())
+		{	
+			event.setCancelled(true);
+			event.getLocation().getWorld().spawnEntity(event.getLocation(), EntityType.CHICKEN).addPassenger(event.getEntity());
+		}
+	}
+}
