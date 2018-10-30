2537c1e44429fbf09a3787633e83054c52b733b4
ikbruneldev <ikbrunel.coding@gmail.com>
2018-10-30 20:14:37 +0100

    Updt GZipFile + new dependency

--- src/main/java/fr/HtSTeam/HtS/Utils/Files/GZipFile.java
+++ src/main/java/fr/HtSTeam/HtS/Utils/Files/GZipFile.java
@@ -6,16 +6,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.GZIPOutputStream;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class GZipFile {
 	
 	private String source_file;
+	private String ext;
 	private String output_gzip_file;
 	
 	public GZipFile(String source_file, String output_gzip_file) {
 		this.source_file = source_file;
-		
+		ext = "." + FilenameUtils.getExtension(source_file) + ".gz";
 		int i = 0;
-		while(new File(output_gzip_file + "-" + i).exists())
+		while(new File(output_gzip_file + "-" + i + ext).exists())
 			i++;
 		this.output_gzip_file = output_gzip_file + "-" + i;
 	}
@@ -23,7 +26,7 @@ public class GZipFile {
 	public void gzipIt(){
 		byte[] buffer = new byte[1024];
 		try {
-			GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream(output_gzip_file + ".gz"));
+			GZIPOutputStream gzos = new GZIPOutputStream(new FileOutputStream(output_gzip_file + ext));
 			FileInputStream in = new FileInputStream(source_file);
 			int len;
 			while ((len = in.read(buffer)) > 0)
