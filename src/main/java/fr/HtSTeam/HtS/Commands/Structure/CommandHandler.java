cc53a91c2e4fbb7577d478b398eb9bf5fd41f7c6
ikbruneldev <ikbrunel.coding@gmail.com>
2018-11-01 11:22:43 +0100

    CommandHandler Documented, did my best

--- src/main/java/fr/HtSTeam/HtS/Commands/Structure/CommandHandler.java
+++ src/main/java/fr/HtSTeam/HtS/Commands/Structure/CommandHandler.java
@@ -6,6 +6,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+
+/**
+ * To add a command:
+ *<br>create a static public method, name it the name of the command</br>
+ *<br>Execution method: it needs to return a boolean and have these parameters : Player sender, org.bukkit.command.Command cmd, String msg, String[] args</br>
+ *<br>Completion method: named it the same as the command with no parameters and returns HashMap<String,ArrayList<String>> representing a tree of tab completion</br>
+ */
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface CommandHandler {
