43f7fda2efb0ad54aad422c9d46b4a5849035581
ikbruneldev <ikbrunel.coding@gmail.com>
2018-11-01 11:12:13 +0100

    Tag Documented

--- src/main/java/fr/HtSTeam/HtS/Utils/Tag.java
+++ src/main/java/fr/HtSTeam/HtS/Utils/Tag.java
@@ -9,6 +9,13 @@ public class Tag {
 	public HashMap<String, String> attributes;
 	public ArrayList<Tag> values;
 	
+	/**
+	 * Type used in XmlFile to create tree node.
+	 * <br>values = null will create a value node of the name</br>
+	 * @param name Not nullable
+	 * @param attributes nullable
+	 * @param values nullable
+	 */
 	public Tag(String name, HashMap<String, String> attributes, ArrayList<Tag> values) {
 		this.name = name;
 		this.attributes = attributes;
