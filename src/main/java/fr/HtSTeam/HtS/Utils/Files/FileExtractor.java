01137caab6d991843c9c7a049f450ba281c257c1
ikbruneldev <ikbrunel.coding@gmail.com>
2018-11-02 12:43:21 +0100

    FileExtractor Fix

--- src/main/java/fr/HtSTeam/HtS/Utils/Files/FileExtractor.java
+++ src/main/java/fr/HtSTeam/HtS/Utils/Files/FileExtractor.java
@@ -8,26 +8,33 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import fr.HtSTeam.HtS.Main;
+
 public class FileExtractor {
 	
 	public static String wdir;
-	public static final String lt = "main/resources/loot_tables/";
-	public static final String cr = "main/resources/crafts/";
+	public static final String lt = "/loot_tables/";
+	public static final String cr = "/crafts/";
 	public static final String Edir = "/minecraft/entities/";
 	public static final String Gdir = "/minecraft/gameplay/";
 	public static final String Rdir = "/minecraft/recipes/";
 	public static final String Cdir = "/customs/";
 	
-	public static void extractFile(String name, String folderPath) throws IOException, URISyntaxException
-    {	
-		String serverPath = FileExtractor.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().split("plugins")[0].split("/")[1];
-		String path = serverPath + folderPath;
+	/**
+	 * Extracts the file from resources folder
+	 * @param name path to extract file
+	 * @param folderPath path to resources file
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public static void extractFile(String name, String folderPath) throws IOException, URISyntaxException {	
+		String path = Main.plugin.getDataFolder() + folderPath;
 		if(!Files.exists(Paths.get(path)))
 			new File(path).mkdirs();
 		
         File target = new File(path + name.split("/")[1]);
         if (target.exists())
-            return;
+			target.delete();
 
         FileOutputStream out = new FileOutputStream(target);
         ClassLoader cl = FileExtractor.class.getClassLoader();
@@ -36,9 +43,7 @@ public class FileExtractor {
         byte[] buf = new byte[8*1024];
         int len;
         while((len = in.read(buf)) != -1)
-        {
             out.write(buf,0,len);
-        }
         out.close();
         in.close();
     }
